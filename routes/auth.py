c66d036b839fa4ab0b06577e6589e2f3c2235354
TanishTyagi26 <tanishtyagi52@gmail.com>
2026-07-12 15:10:39 +0530

    Add dynamic authentication pages using Flask routing

--- routes/auth.py
+++ routes/auth.py
@@ -1,3 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
+
+@auth.route("/login/<role>")
+def login(role):
+    return render_template("login.html", role=role.title())
