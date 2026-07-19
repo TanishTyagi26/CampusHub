36a0a6a48060925da733119b989f0aa03f31120e
TanishTyagi26 <tanishtyagi52@gmail.com>
2026-07-19 16:23:58 +0530

    Added student registration feature

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,39 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+import sqlite3
 
 auth = Blueprint("auth", __name__)
 
+# Login Page
 @auth.route("/login/<role>")
 def login(role):
     return render_template("login.html", role=role.title())
+
+
+# Registration Page
+@auth.route("/register")
+def register():
+    return render_template("register.html")
+
+
+# Save Student Data
+@auth.route("/register", methods=["POST"])
+def register_student():
+
+    full_name = request.form["full_name"]
+    email = request.form["email"]
+    password = request.form["password"]
+    branch = request.form["branch"]
+    year = request.form["year"]
+
+    connection = sqlite3.connect("database/campushub.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    INSERT INTO students(full_name,email,password,branch,year)
+    VALUES(?,?,?,?,?)
+    """, (full_name, email, password, branch, year))
+
+    connection.commit()
+    connection.close()
+
+    return redirect("/login/student")
