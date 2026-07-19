aef9db90e396460d96e1b579e7d6690e1f7bad0b
TanishTyagi26 <tanishtyagi52@gmail.com>
2026-07-19 16:35:29 +0530

    Added student dashboard

--- routes/student.py
+++ routes/student.py
@@ -4,4 +4,8 @@ student = Blueprint("student", __name__)
 
 @student.route("/student")
 def student_home():
-    return render_template("login.html")
+    return render_template("login.html", role="Student")
+
+@student.route("/student/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
