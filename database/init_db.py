3796d1e35bbd6d353b49b3ae3d4b1636aea7da3a
TanishTyagi26 <tanishtyagi52@gmail.com>
2026-07-19 22:28:31 +0530

    Added faculty authentication system

--- database/init_db.py
+++ database/init_db.py
@@ -32,6 +32,16 @@ CREATE TABLE IF NOT EXISTS announcements(
 )
 """)
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS faculty(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    full_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    department TEXT NOT NULL
+)
+""")
+
 connection.commit()
 connection.close()
 
--- routes/faculty.py
+++ routes/faculty.py
@@ -1,7 +1,65 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, session
+import sqlite3
 
 faculty = Blueprint("faculty", __name__)
 
-@faculty.route("/faculty")
-def faculty_home():
+
+# Faculty Login
+@faculty.route("/faculty", methods=["GET", "POST"])
+def faculty_login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        connection = sqlite3.connect("database/campushub.db")
+        cursor = connection.cursor()
+
+        cursor.execute("""
+        SELECT * FROM faculty
+        WHERE email=? AND password=?
+        """, (email, password))
+
+        user = cursor.fetchone()
+
+        print("Email:", email)
+        print("Password:", password)
+        print("User:", user)
+
+
+
+        connection.close()
+
+        if user:
+
+            session["faculty_id"] = user[0]
+            session["faculty_name"] = user[1]
+
+            return redirect("/faculty/dashboard")
+
+        return "Invalid Email or Password"
+
     return render_template("faculty_login.html")
+
+
+# Faculty Dashboard
+@faculty.route("/faculty/dashboard")
+def faculty_dashboard():
+
+    if "faculty_id" not in session:
+        return redirect("/faculty")
+
+    return render_template(
+        "faculty_dashboard.html",
+        faculty_name=session["faculty_name"]
+    )
+
+
+# Faculty Logout
+@faculty.route("/faculty/logout")
+def faculty_logout():
+
+    session.clear()
+
+    return redirect("/")
